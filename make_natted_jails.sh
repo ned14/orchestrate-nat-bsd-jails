06034e637954237451017c9367a58c99cf434308
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere>
2015-02-07 19:23:00 +0000

    Replace loopback networking with VIMAGE experimental networking. Correctly got it configuring the network jail side, still to do configure of NAT on host side.

--- make_natted_jails.sh
+++ make_natted_jails.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 # Create X NAT routed BSD jails
 # (C) 2015 MaidSafe - Niall Douglas
 # Created: Feb 2015
@@ -30,35 +30,40 @@ sysctl net.inet.ip.forwarding=1
 # What is the IP of em0?
 HOSTIPADDR=$(ifconfig em0 | grep "inet " | cut -f 2 -d ' ' -)
 
-echo 'include "/etc/pf.conf"' > pf.conf
-for N in $(seq 1 $TOTALJAILS)
-do
-  echo "Configuring simulated network stack $N of $TOTALJAILS ..."
-  JAILNAME=$(printf "lo7%03s" "$N")
-  rm -rf /usr/jails/$JAILNAME
-  # Create a custom loopback device for the jail
-  ifconfig $JAILNAME create
-  ifconfig $JAILNAME inet 10.77.$N.1 netmask 255.255.255.0
-  # Configure a nat on custom loopback device
-  echo "nat pass on em0 from 10.77.$N.1 to any -> $HOSTIPADDR" >> pf.conf
-done
-pfctl -f pf.conf
-
 for N in $(seq 1 $TOTALJAILS)
 do
   echo "Creating jail $N of $TOTALJAILS ..."
-  JAILNAME=$(printf "lo7%03s" "$N")
+  JAILNAME=$(printf "natjail%03s" "$N")
+  EPAIRNAME=$(printf "epair%s" "$N")
+  # Create a virtual ethernet patch cable (I joke not ...)
+  ifconfig $EPAIRNAME create up
+  # That creates two if devices, epair$Na for the host side and epair$Nb for the jail side
   # Create the jail from my flavour
-  ezjail-admin create -f make_natted_jails $JAILNAME $JAILNAME\|10.77.$N.1 2> /dev/null
-  # Enable raw sockets for the jail
-  echo "export jail_${JAILNAME}_parameters=\"allow.raw_sockets\"" >> /usr/local/etc/ezjail/$JAILNAME
+  ezjail-admin create -f make_natted_jails $JAILNAME x 2> /dev/null
+  # Enable own network stack for the jail
+  echo "export jail_${JAILNAME}_parameters=\"vnet vnet.interface=${EPAIRNAME}b allow.raw_sockets\"" >> /usr/local/etc/ezjail/$JAILNAME
   
   # Copy files into /usr/jails/jail77N/root
   cp -a runonboot /usr/jails/$JAILNAME/etc/rc.d/runonboot
   
   # Start the jail
   ezjail-admin start $JAILNAME
+  
+  # Configure the network stack for the jail
+  jexec $JAILNAME /sbin/ifconfig ${EPAIRNAME}b inet 10.77.$N.1 up
+  jexec $JAILNAME /sbin/route add default ${HOSTIPADDR}
 done
+
+#echo 'include "/etc/pf.conf"' > pf.conf
+#for N in $(seq 1 $TOTALJAILS)
+#do
+#  echo "Configuring simulated network stack $N of $TOTALJAILS ..."
+#  JAILNAME=$(printf "natjail%03s" "$N")
+#  rm -rf /usr/jails/$JAILNAME
+#  # Configure a nat on custom loopback device
+#  echo "nat pass on em0 from 10.77.$N.1 to any -> $HOSTIPADDR" >> pf.conf
+#done
+#pfctl -f pf.conf
 
 echo "Press Return to clean up ..."
 read FOO
@@ -66,14 +71,15 @@ read FOO
 for N in $(seq 1 $TOTALJAILS)
 do  
   echo "Deleting jail $N of $TOTALJAILS ..."
-  JAILNAME=$(printf "lo7%03s" "$N")
+  JAILNAME=$(printf "natjail%03s" "$N")
+  EPAIRNAME=$(printf "epair%s" "$N")
   echo "Ping log was:"
   jexec $JAILNAME /usr/bin/killall ping
   jexec $JAILNAME /bin/cat /root/pinglog
   ezjail-admin stop $JAILNAME
   ezjail-admin delete $JAILNAME
   rm -rf /usr/jails/$JAILNAME
-  ifconfig $JAILNAME destroy
+  ifconfig ${EPAIRNAME}a destroy
 done
 # Flush the NAT table entries
 pfctl -F nat
